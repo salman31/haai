918a5cac0c0d102261a12fcdadf53faaf0e5e046
Salman <salman31@rocketmail.com>
2023-07-10 21:53:08 +0700

    add register page

--- dist/app.js
+++ dist/app.js
@@ -2,18 +2,22 @@
 fetch('./data.json')
     .then(response => response.json())
     .then(data => {
-        const features = document.getElementById('features').innerHTML;
-        const compiledfeatures = Handlebars.compile(features);
-        document.getElementById('list-features').innerHTML = compiledfeatures(data);
+        const features = document.getElementById('features');
+        if(features){
+            const compiledfeatures = Handlebars.compile(features.innerHTML);
+            document.getElementById('list-features').innerHTML = compiledfeatures(data);
+        }
 });
 
 // faq
 fetch('./data.json')
     .then(response => response.json())
     .then(data => {
-        const faq = document.getElementById('faq').innerHTML;
-        const compiledFaq = Handlebars.compile(faq);
-        document.getElementById('list-faq').innerHTML = compiledFaq(data);
+        const faq = document.getElementById('faq');
+        if (faq) {
+            const compiledFaq = Handlebars.compile(faq.innerHTML);
+            document.getElementById('list-faq').innerHTML = compiledFaq(data);
+        }
 });
 
 // menu mobile
@@ -21,20 +25,22 @@ const menuToggle = document.getElementById('js-menu-toggle');
 const menu = document.getElementById('js-menu');
 const body = document.body;
 
-menuToggle.addEventListener('click', (e) => {
-    menu.classList.toggle('hidden');
-});
-menu.addEventListener('click', (e) => {
-    menu.classList.toggle('hidden');
-});
-body.addEventListener('click', (event) => {
-    const isClickInsideContainer = menu.contains(event.target);
-    const isClickOnToggle = event.target === menuToggle;
+if (menuToggle) {
+    menuToggle.addEventListener('click', (e) => {
+        menu.classList.toggle('hidden');
+    });
+    menu.addEventListener('click', (e) => {
+        menu.classList.toggle('hidden');
+    });
+    body.addEventListener('click', (event) => {
+        const isClickInsideContainer = menu.contains(event.target);
+        const isClickOnToggle = event.target === menuToggle;
 
-    if (!isClickInsideContainer && !isClickOnToggle) {
-        menu.classList.add('hidden');
-    }
-});
+        if (!isClickInsideContainer && !isClickOnToggle) {
+            menu.classList.add('hidden');
+        }
+    });
+}
 
 // menu scroll
 function scrollToSection(sectionId) {
